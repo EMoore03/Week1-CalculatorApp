9041ee4b10d931698946a70a0f5507c05dbc2c0e
ethan <ethan@DESKTOP-10L1M6N>
2024-09-27 13:22:53 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//int firstNumber = 5;
+//int secondNumber = 20;
+//int result = firstNumber + secondNumber;
+//Console.WriteLine("The result of the two numbers is {0} ", result);
+//Console.ReadKey();
+
+Console.WriteLine("Type in the first number followed by the enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number followed by the enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
